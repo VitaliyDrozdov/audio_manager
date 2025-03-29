329c70365458a18ea270e73f44edb91eb58ae733
VitaliyDrozdov <vitdrzdv@mail.ru>
2025-03-29 23:22:30 +0300

    add db, settings

---
+++ src/infra/db_accessor.py
@@ -0,0 +1,11 @@
+# import os
+
+
+# from typing import AsyncGenerator
+
+# from sqlalchemy.ext.asyncio import (
+#     AsyncSession,
+#     async_sessionmaker,
+#     create_async_engine,
+# )
+# from dotenv import load_dotenv
---
+++ src/settings.py
@@ -0,0 +1,26 @@
+import os
+from typing import Literal
+
+from dotenv import load_dotenv
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env", env_ignore_empty=True, extra="ignore"
+    )
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    SECRET_KEY: str
+
+    POSTGRES_DB: str | None = os.getenv("POSTGRES_DB")
+    POSTGRES_USER: str | None = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str | None = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT", 5432))
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    SUPERUSER_EMAIL: EmailStr
+    SUPERUSER_PASSWORD: str
