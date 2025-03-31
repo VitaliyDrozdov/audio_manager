644c46c3cc798efd361aaf47986f7d72024befff
VitaliyDrozdov <vitdrzdv@mail.ru>
2025-03-31 05:48:28 +0300

    refactor db_accessor

--- src/infra/db_accessor.py
+++ src/infra/db_accessor.py
@@ -1,5 +1,6 @@
-from typing import AsyncGenerator
+from typing import Annotated, AsyncGenerator
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -11,16 +12,23 @@ from src.settings import settings
 
 
 class DBConfig:
-    DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"  # noqa E501
-    engine = create_async_engine(DATABASE_URL, echo=True)
-    AsyncSession_ = async_sessionmaker(
-        bind=engine, expire_on_commit=False, autoflush=False, autocommit=False
-    )
+    def __init__(self):
+        self.DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"  # noqa E501
+        self.engine = create_async_engine(self.DATABASE_URL, echo=True)
+        self.AsyncSession_ = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    async def get_db(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.AsyncSession_() as session:
+            yield session
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with DBConfig.AsyncSession_() as session:
-        yield session
+db_config = DBConfig()
+DBSession = Annotated[AsyncSession, Depends(db_config.get_db)]
 
 
 class Base(DeclarativeBase):
--- src/main.py
+++ src/main.py
@@ -6,4 +6,4 @@ app = FastAPI(title="audio_manager", summary="API_v1")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", reload=True, port=8000)
+    uvicorn.run("main:app", reload=True, port=8100)
